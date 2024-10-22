4f37112f144ba42acf0be530ff3735976ace1817
danie <danie@LAPTOP-EHHD3LP9>
2024-10-22 17:25:23 +0200

    3 Manipulation des objets (par leur référence)

---
+++ TP2_manip_GIREL/src/main/java/com/mycompany/tp2_manip_girel/Moussaka.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tp2_manip_girel;
+
+/**
+ *
+ * @author danie
+ */
+public class Moussaka {
+    int nbCalories;
+    public Moussaka (int calories){
+        this.nbCalories = calories;
+    }
+    public int getNbCalories() {
+        return nbCalories;
+}
+}
+                
+
+
+
---
+++ TP2_manip_GIREL/src/main/java/com/mycompany/tp2_manip_girel/Tartiflette.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tp2_manip_girel;
+
+/**
+ *
+ * @author danie
+ */
+public class Tartiflette {
+    int nbCalories;
+    public Tartiflette(int Calories){
+        this.nbCalories = Calories;
+    }}
