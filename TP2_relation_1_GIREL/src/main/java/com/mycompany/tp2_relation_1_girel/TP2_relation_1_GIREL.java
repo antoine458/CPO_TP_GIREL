ff33907be5ac6a3baeef69d67195e5fab844e4c5
danie <danie@LAPTOP-EHHD3LP9>
2024-10-23 12:32:22 +0200

    4 Coder une première relation entre deux objets

--- TP2_relation_1_GIREL/src/main/java/com/mycompany/tp2_relation_1_girel/TP2_relation_1_GIREL.java
+++ TP2_relation_1_GIREL/src/main/java/com/mycompany/tp2_relation_1_girel/TP2_relation_1_GIREL.java
@@ -13,5 +13,28 @@ public class TP2_relation_1_GIREL {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Voiture uneClio = new Voiture ("Clio williams", "Renault", 150 ) ; 
+        Voiture uneAutreClio = new Voiture ("Clio Maxi kit car", "Renault", 250 ) ; 
+        Voiture une2008 = new Voiture ("Escort MkII", "Ford", 283 ) ; 
+        Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ; 
+        Personne bob = new Personne("Bobby", "Sixkiller"); 
+        Personne reno = new Personne("Reno", "Raines"); 
+//System.out.println("liste des voitures disponibles "+ uneClio  + 
+//"\n" + uneAutreClio   + "\n" + une2008   + "\n" + uneMicra ) ;
+    bob.Lstvoiture[0] = uneClio ; 
+    bob.nbvoiture = 1 ; 
+    uneClio.Proprietaire = bob ;
+    reno.Lstvoiture[1] = une2008 ; 
+    reno.nbvoiture = 1 ; 
+    une2008.Proprietaire = bob ;
+    bob.Lstvoiture[2] = uneAutreClio ; 
+    bob.nbvoiture = 2 ; 
+    uneAutreClio.Proprietaire = bob ;
+    System.out.println("la premiere voiture de Bob est " + 
+bob.Lstvoiture[0] ) ; 
+    System.out.println("la premiere voiture de reno est " + 
+reno.Lstvoiture[1] );
+            System.out.println("la deuxieme voiture de Bob est " + 
+bob.Lstvoiture[2] );
     }
 }
